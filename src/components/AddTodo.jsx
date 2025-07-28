4e7c020f6b6de52fc9b3a0f3e264a1310e0c2f6a
田中 想一郎 <soichiro.tanaka@mugen-corp.jp>
2025-07-28 18:52:22 +0900

    タスクを追加画面のcss調整

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -3,39 +3,39 @@ export const AddTodo = (props) => {
   return (
 
      <div className='container'>
-          <div className='showaddform-title'>
-            <h1>タスクを追加</h1>
-          </div>
-          <div className='showaddform-name'>
-            <p className='todo-title'>タスク名</p>
-            <input
-              type="text"
-              className="showaddform-taskname"
-              placeholder='タスク名を入力'
-              value={todoTitle}
-              onChange={onChangeTodoTitle}
-            />
-          </div>
-          <div>
-            <p>タスクの詳細</p>
-            <textarea
-              placeholder="タスクの詳細を入力"
-              className='showaddform-taskdetail'
-              value={todoDetail}  
-              onChange={onChangeTododetail}
-            />
-          </div>
-          <div>
-            <input 
-              type="date" 
-              value={todoDeadline}
-              onChange={onChangeTodoDeadline}
-            />
-          </div>
-          <div className="addshowform-button-area">
-            <button onClick={onClickAdd}>タスクを追加</button>
-          </div> 
+        <div className='showaddform-title'>
+          <h1>タスクを追加</h1>
         </div>
+        <div className='showaddform-name'>
+          <p className='todo-title'>タスク名</p>
+          <input
+            type="text"
+            className="showaddform-taskname"
+            placeholder='タスク名を入力'
+            value={todoTitle}
+            onChange={onChangeTodoTitle}
+          />
+        </div>
+        <div>
+          <p className="todo-title-detail">タスクの詳細</p>
+          <textarea
+            placeholder="タスクの詳細を入力"
+            className='showaddform-taskdetail'
+            value={todoDetail}  
+            onChange={onChangeTododetail}
+          />
+        </div>
+        <div className='showaddform-deadline'>
+          <input 
+            type="date" 
+            value={todoDeadline}
+            onChange={onChangeTodoDeadline}
+          />
+        </div>
+        <div className="addshowform-button-area">
+          <button onClick={onClickAdd}>タスクを追加</button>
+        </div> 
+    </div>
 
 
   )
